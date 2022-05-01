8fd5d11c5a3c9b21e57d23123ce5859a8fccbaf8
gek64 <10346089+gek64@users.noreply.github.com>
2022-05-01 15:39:14 +0800

    Add files via upload

---
+++ trunk/configs/boards/HC5761/board.h
@@ -0,0 +1,30 @@
+/* HiWiFi HC5761 */
+
+#define BOARD_PID	"HC5761"
+#define BOARD_NAME	"HiWiFi HC5761"
+#define BOARD_DESC	"HiWiFi HC5761 Wireless Router"
+#define BOARD_VENDOR_NAME	"Beijing Jike Jike Technology Co., Ltd."
+#define BOARD_VENDOR_URL	"https://www.hiwifi.com/"
+#define BOARD_MODEL_URL	"https://www.hiwifi.com/"
+#define BOARD_BOOT_TIME	30
+#define BOARD_FLASH_TIME	90
+#define BOARD_GPIO_BTN_RESET	12
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	7
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	11
+#undef  BOARD_GPIO_LED_USB
+#define BOARD_GPIO_LED_ROUTER	9
+#define BOARD_GPIO_PWR_USB	13
+#define BOARD_GPIO_PWR_USB_ON	1
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	1
+#define BOARD_NUM_ANT_5G_RX	1
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
